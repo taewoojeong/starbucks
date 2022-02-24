d257a4986e05043d02c2c55f8f68b4b4af574640
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-02-24 15:35:20 +0900

    2022-02-23

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ const badgeEl = document.querySelector('header .badges');
 window.addEventListener(
 	'scroll',
 	_.throttle(function () {
-		console.log(window.scrollY);
 		if (window.scrollY > 500) {
 			// 배지 숨기기
 			gsap.to(badgeEl, 0.6, {
@@ -38,3 +37,80 @@ window.addEventListener(
 );
 // _.throttle(함수, 시간)
 // gsap.to(요소, 지속시간, 옵션);
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function (fadeEl, index) {
+	gsap.to(fadeEl, 1, {
+		delay: (index + 1) * 0.7,
+		opacity: 1,
+	});
+});
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+	direction: 'vertical',
+	autoplay: true,
+	loop: true,
+});
+
+new Swiper('.promotion .swiper-container', {
+	slidesPerView: 3,
+	spaceBetween: 10,
+	centeredSlides: true,
+	loop: true,
+	// autoplay: {
+	// 	delay: 5000,
+	// },
+	pagination: {
+		el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+		clickable: true, // 사용자 페이지 번호 요소 제어 가능 여부
+	},
+	navigation: {
+		prevEl: '.promotion .swiper-prev',
+		nextEl: '.promotion .swiper-next',
+	},
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+const btnMaterialIcons = promotionToggleBtn.querySelector('.material-icons');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+	isHidePromotion = !isHidePromotion;
+	if (isHidePromotion) {
+		// 숨김 처리!
+		promotionEl.classList.add('hide');
+		btnMaterialIcons.textContent = 'download';
+	} else {
+		// 보임 처리!
+		promotionEl.classList.remove('hide');
+		btnMaterialIcons.textContent = 'upload';
+	}
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+	// `.toFixed()`를 통해 반환된 문자 데이터를,
+	// `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+	return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+	// gsap.to(요소, 시간, 옵션)
+	gsap.to(
+		selector, // 선택자
+		random(1.5, 2.5), // 애니메이션 동작 시간
+		{
+			// 옵션
+			y: size,
+			repeat: -1,
+			yoyo: true,
+			ease: Power1.easeInOut,
+			delay: random(0, delay),
+		}
+	);
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
